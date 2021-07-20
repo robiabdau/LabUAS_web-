e5eac2c9bca5a633544b7b9b44724801bfd96ea8
Robi Abda'u <81896427+robiabdau@users.noreply.github.com>
2021-07-20 22:48:43 +0700

    Add files via upload

---
+++ ci4.sql
@@ -0,0 +1,109 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 20, 2021 at 05:48 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ci4`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `iuran`
+--
+
+CREATE TABLE `iuran` (
+  `id` int(11) NOT NULL,
+  `keterangan` tinytext NOT NULL,
+  `tanggal` date NOT NULL,
+  `bulan` int(2) NOT NULL,
+  `tahun` year(4) NOT NULL,
+  `jumlah` decimal(10,2) NOT NULL,
+  `warga_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `iuran`
+--
+
+INSERT INTO `iuran` (`id`, `keterangan`, `tanggal`, `bulan`, `tahun`, `jumlah`, `warga_id`) VALUES
+(1, 'Belum Bayar', '2021-07-20', 10, 2021, '0.00', 1),
+(2, 'Belum Bayar', '2021-07-20', 7, 2021, '0.00', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `warga`
+--
+
+CREATE TABLE `warga` (
+  `id` int(11) NOT NULL,
+  `nik` varchar(50) NOT NULL,
+  `nama` varchar(200) NOT NULL,
+  `kelamin` enum('L','P') NOT NULL,
+  `alamat` tinytext NOT NULL,
+  `no_rumah` varchar(10) NOT NULL,
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `warga`
+--
+
+INSERT INTO `warga` (`id`, `nik`, `nama`, `kelamin`, `alamat`, `no_rumah`, `status`) VALUES
+(1, '3213131008980003', 'Robi Abda\'u', 'L', 'Cikarang Mentrang Residence', 'B10', 0),
+(2, '3213130806980002', 'Rizal Gunawan', 'L', 'Cikarang Mentrang Residence', 'A03', 1),
+(3, '3213120603950009', 'Renata Septiani', 'P', 'Cikarang Mentrang Residence', 'C10', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `iuran`
+--
+ALTER TABLE `iuran`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `warga_id` (`warga_id`);
+
+--
+-- Indexes for table `warga`
+--
+ALTER TABLE `warga`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `iuran`
+--
+ALTER TABLE `iuran`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `warga`
+--
+ALTER TABLE `warga`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
